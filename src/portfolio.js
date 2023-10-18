0010ec4f9360ffe857fa5e2f1215031bf3f355f2
HyunwookL <gusdnr0916@gmail.com>
2023-10-18 17:38:18 +0900

    Update domain-related files

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://hyunwookl.github.io',
+  title: 'Hyunwook Lee',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Hyunwook Lee',
+  role: 'Ph.D Student',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: 'https://hyunwookl/github.io/',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/hyunwook-lee-2b15ba283',
+    github: 'https://github.com/HyunwookL',
   },
 }
 
